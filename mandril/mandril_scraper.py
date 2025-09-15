c6bc131eca95e35486ce821dbbf5d26714cd72b8
Sasha <sasha.friis@icloud.com>
2025-09-15 14:11:53 +0200

    succesfully pulled the link and data

--- mandril/mandril_scraper.py
+++ mandril/mandril_scraper.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 from io import StringIO
 
-url = "https://en.wikipedia.org/wiki/List_of_James_Bond_films"
+url = "https://da.wikipedia.org/wiki/Figurer_fra_Casper_%26_Mandrilaftalen"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
     "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -12,3 +12,6 @@ headers = {
 response = requests.get(url, headers=headers)
 
 mandril_tables = pd.read_html(StringIO(response.text))
+mandril_data = mandril_tables[1].convert_dtypes()
+
+print(mandril_data)
